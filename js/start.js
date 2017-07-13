6a7b97b6c6ba718f89674ab5fdedf388b2761fef
Vivek Strom <vivekstorm4@gmail.com>
2017-07-13 23:16:15 +0530

    initial commit

---
+++ js/start.js
@@ -0,0 +1,7 @@
+$( document ).ready(function(){
+   $(".button-collapse").sideNav();
+    $('.slider').slider();
+	  $('.parallax').parallax();
+	    $('.modal').modal();
+  });
+// JavaScript Document
