107c9e5882ef115557f631b78a36ec57de3b3e25
Vivek <vivekstorm4@gmail.com>
2017-09-23 21:43:48 +0530

    Update start.js

--- js/start.js
+++ js/start.js
@@ -1,6 +1,5 @@
 $( document ).ready(function(){
-   $(".button-collapse").sideNav();
-    $('.slider').slider();
-	  $('.parallax').parallax();
-	     });
-// JavaScript Document
+ $(".button-collapse").sideNav();
+$('.slider').slider();
+$('.parallax').parallax();
+});
